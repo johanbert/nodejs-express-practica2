c7c4af4db65a33a2da3fb1d63786d1ee11a5378d
johanbert <johanbertmosquera@gmail.com>
2021-10-26 19:25:57 -0500

    API Endpoint Delete completed

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const { v1: uuidv1 } = require('uuid');
 const express = require('express')
+const helmet = require('helmet')
 const app = express()
 const port = 3000
+    // DATABASE IN MEMORY
 const DB_USERS = []
+
+app.use(helmet());
+// ENDPOINTS
 
 // Parse URL-encoded bodies (as sent by HTML forms)
 app.use(express.urlencoded({ extended: true }));
@@ -10,25 +15,21 @@ app.use(express.urlencoded({ extended: true }));
 // Parse JSON bodies (as sent by API clients)
 app.use(express.json());
 
-app.get('/', (req, res) => { // RECURSO /
-    res.send('Hola mundo! El básico get')
-})
-
+// GET /users
 app.get('/users', (req, res) => { // RECURSO /users
     res.json(DB_USERS)
 })
 
-app.post('/', (req, res) => {
-    res.send('Hola mundo!, recibí una peticion o request POST')
-})
-
+// POST /users
 app.post('/users', (req, res) => { // RECURSO /users
     req.body.id = uuidv1()
     DB_USERS.push(req.body)
     res.status(201).json({ id: req.body.id, message: 'RECURSO CREADO' })
 })
 
+// PUT /users/:id
 app.put('/users/:id', (req, res) => {
+
     // const body = req.body;
     const { body } = req;
     body.id = req.params.id
@@ -47,11 +48,13 @@ app.put('/users/:id', (req, res) => {
     res.status(200).json({ OK: true })
 })
 
+// PATCH /users/:id
 app.patch('/users/:id', (req, res) => {
+
     const { body } = req;
     delete body.id;
     const response = DB_USERS.find((obj, i) => {
-        return obj.id != req.params.id ? false : Array.from(Object.keys(body)).map((key) => DB_USERS[i][key] = body[key])
+        return obj.id != req.params.id ? false : Object.keys(body).map((key) => DB_USERS[i][key] = body[key])
     })
 
     if (!response)
@@ -59,8 +62,17 @@ app.patch('/users/:id', (req, res) => {
     res.status(200).json({ OK: true })
 })
 
-app.delete('/', (req, res) => {
-    res.send('Hola mundo!, recibí una peticion o request DELETE')
+// DELETE /users/:id
+
+app.delete('/users/:id', (req, res) => {
+    // req.params.id
+
+    const response = DB_USERS.find((obj, i) => obj.id === req.params.id ? DB_USERS.splice(i, 1) : false)
+
+    if (!response)
+        return res.status(404).json({ message: `NO SE ENCONTRÓ EL ID:${req.params.id}` })
+    res.status(200).json({ OK: true })
 })
 
-const server = app.listen(port, () => console.log('Server ready', port))
+
+app.listen(port, () => console.log('Server ready', port))
